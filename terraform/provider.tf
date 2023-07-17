e5aa3c0b849ace02e9dadb560878dd4777ee874c
Guy Peer <guypeer1985@gmail.com>
2023-07-17 23:44:28 +0300

    init 9

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-east-1"
-  access_key = "AKIA4LD7OFQOTO5ZFSEU"
-  secret_access_key = "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"
+  region        = "us-east-1"
+  access_key    = "AKIA4LD7OFQOTO5ZFSEU"
+  secret_key    = "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"
 }
