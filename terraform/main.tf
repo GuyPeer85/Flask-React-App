48af20aa479b9f06283fb2759e0831ebac223db5
Guy Peer <guypeer1985@gmail.com>
2023-07-18 17:53:37 +0300

    finish server side 9

--- terraform/main.tf
+++ terraform/main.tf
@@ -81,3 +81,20 @@ resource "aws_lb_listener" "flask_react_app_listener" {
     target_group_arn = aws_lb_target_group.flask_react_app_tg.arn
   }
 }
+
+# Attach Task Definition to Cluster
+resource "aws_ecs_service" "flask_react_app_service" {
+  name            = "flask-react-app-service"
+  cluster         = aws_ecs_cluster.flask_react_app_cluster.id
+  task_definition = aws_ecs_task_definition.flask_react_app_task_definition.arn
+  desired_count   = 1
+
+  deployment_minimum_healthy_percent = 100
+  deployment_maximum_percent         = 200
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.flask_react_app_tg.arn
+    container_name   = "flask-react-app-container"
+    container_port   = 5000
+  }
+}
