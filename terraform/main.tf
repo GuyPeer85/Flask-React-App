7655f9a2e7ffd5a372910f958821428e407eae4e
Guy Peer <guypeer1985@gmail.com>
2023-07-18 17:51:50 +0300

    finish server side 8

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,25 +3,25 @@ resource "aws_security_group" "flask_react_app_sg" {
   name        = "flask-react-app-SG"
   description = "flask-react-app-SG"
 
-  egress {
-    cidr_blocks = ["0.0.0.0/0"]
+  ingress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    security_groups = [aws_security_group.flask_react_app_sg.id]
+  }
+
+  ingress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
+    self = true
   }
 
   ingress {
-    cidr_blocks = ["0.0.0.0/0"]
-    from_port   = 80
-    to_port     = 80
+    from_port   = 0
+    to_port     = 65535
     protocol    = "tcp"
-  }
-
-  ingress {
     cidr_blocks = ["0.0.0.0/0"]
-    from_port   = 5000
-    to_port     = 5000
-    protocol    = "tcp"
   }
 }
 
@@ -45,11 +45,6 @@ resource "aws_ecs_task_definition" "flask_react_app_task_definition" {
       "image": "848523308061.dkr.ecr.us-east-1.amazonaws.com/flask-react-app-repo-ecr:latest",
       "portMappings": [
         {
-          "containerPort": 80,
-          "hostPort": 80,
-          "protocol": "tcp"
-        },
-        {
           "containerPort": 5000,
           "hostPort": 5000,
           "protocol": "tcp"
@@ -62,7 +57,7 @@ resource "aws_ecs_task_definition" "flask_react_app_task_definition" {
 # EC2 Target Group
 resource "aws_lb_target_group" "flask_react_app_tg" {
   name        = "flask-react-app-TG"
-  port        = 80
+  port        = 5000
   protocol    = "HTTP"
   target_type = "ip"
   vpc_id      = "vpc-08d9d0b1dc4b47d41"
@@ -84,27 +79,5 @@ resource "aws_lb_listener" "flask_react_app_listener" {
   default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.flask_react_app_tg.arn
-  }
-}
-
-# ECS Service
-resource "aws_ecs_service" "flask_react_app_service" {
-  name            = "flask-react-app-service"
-  cluster         = aws_ecs_cluster.flask_react_app_cluster.id
-  task_definition = aws_ecs_task_definition.flask_react_app_task_definition.arn
-  desired_count   = 1
-
-  deployment_minimum_healthy_percent = 100
-  deployment_maximum_percent         = 200
-
-  network_configuration {
-    subnets         = ["subnet-012ef49073ec21b34", "subnet-018a531e030fc761b", "subnet-0ed855ff1f7d5fb61", "subnet-09f48fe3de3534711", "subnet-0ebda44417a1904a9", "subnet-0b0fc1ed5fa23a11b"]
-    security_groups = [aws_security_group.flask_react_app_sg.id]
-  }
-
-  load_balancer {
-    target_group_arn = aws_lb_target_group.flask_react_app_tg.arn
-    container_name   = "flask-react-app-container"
-    container_port   = 80
   }
 }
