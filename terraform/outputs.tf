67213e7c6cf052f206c89e5397dd22aeb21216a3
Guy Peer <guypeer1985@gmail.com>
2023-07-18 23:01:47 +0300

    apply output.tf 8

--- terraform/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "service_ipv4" {
-  value = aws_ecs_service.flask_react_app_service.service_registries[0].container_name
-}
-
-output "service_dns_name" {
-  value = aws_lb.flask_react_app_alb.dns_name
-}
