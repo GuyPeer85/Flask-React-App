ce1a09d2a59cb3ea58e48916d99b994888a0a64a
Guy Peer <guypeer1985@gmail.com>
2023-07-18 13:59:50 +0300

    fix exist repo 3

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -1,5 +1,11 @@
 # ECR Repository
 resource "aws_ecr_repository" "flask_react_app_repo_ecr" {
-  name = "flask-react-app-repo-ecr"
-  image_tag_mutability = "MUTABLE"
+  count                 = var.create_ecr_repository ? 1 : 0
+  name                  = "flask-react-app-repo-ecr"
+  image_tag_mutability  = "MUTABLE"
+}
+
+# Output the ECR repository URL
+output "ecr_repository_url" {
+  value = aws_ecr_repository.flask_react_app_repo_ecr[0].repository_url
 }
