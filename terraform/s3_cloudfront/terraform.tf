efaf2c0a2ba1324d78b31362768d543c1785bc22
Guy Peer <guypeer1985@gmail.com>
2023-07-19 21:43:15 +0300

    test 5

--- terraform/s3_cloudfront/terraform.tf
+++ terraform/s3_cloudfront/terraform.tf
@@ -41,22 +41,29 @@ POLICY
 }
 
 # Cloudfront Distributor
+resource "aws_cloudfront_origin_access_identity" "oai" {
+  comment = "OAI for S3 bucket"
+}
+
 resource "aws_cloudfront_distribution" "s3_distribution" {
   origin {
-    domain_name = aws_s3_bucket.bucket.website_endpoint
+    domain_name = aws_s3_bucket.bucket.bucket_regional_domain_name
     origin_id   = "S3Origin"
+
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.oai.cloudfront_access_identity_path
+    }
   }
 
   enabled             = true
   is_ipv6_enabled     = true
-  comment             = "S3 bucket distribution"
+  comment             = "CloudFront Distribution for S3 bucket"
   default_root_object = "index.html"
 
   default_cache_behavior {
-    viewer_protocol_policy = "redirect-to-https"
-    allowed_methods        = ["GET", "HEAD", "OPTIONS"]
-    cached_methods         = ["GET", "HEAD"]
-    target_origin_id       = "S3Origin"
+    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "S3Origin"
 
     forwarded_values {
       query_string = false
@@ -65,6 +72,11 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
         forward = "none"
       }
     }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
   }
 
   restrictions {
@@ -76,4 +88,6 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
   viewer_certificate {
     cloudfront_default_certificate = true
   }
+
+  price_class = "PriceClass_100"
 }
