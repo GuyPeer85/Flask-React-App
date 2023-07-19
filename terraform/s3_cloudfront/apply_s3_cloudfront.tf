3b37bef39103c1948b50eb3149cfd2efdf207491
Guy Peer <guypeer1985@gmail.com>
2023-07-19 17:57:28 +0300

    trigger the apply 3

---
+++ terraform/s3_cloudfront/apply_s3_cloudfront.tf
@@ -0,0 +1,66 @@
+provider "aws" {
+  region     = "us-east-1"
+  access_key = "<AWS_ACCESS_KEY>"
+  secret_key = "<AWS_SECRET_KEY>"
+}
+
+resource "aws_s3_bucket" "bucket" {
+  bucket = "alex-home-hands-on"
+  acl    = "public-read"
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+      {
+          "Sid": "PublicReadGetObject",
+          "Effect": "Allow",
+          "Principal": "*",
+          "Action": "s3:GetObject",
+          "Resource": "arn:aws:s3:::alex-home-hands-on/*"
+      }
+  ]
+}
+POLICY
+
+  website {
+    index_document = "index.html"
+  }
+}
+
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  origin {
+    domain_name = aws_s3_bucket.bucket.bucket_regional_domain_name
+    origin_id   = "S3Origin"
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  comment             = "S3 bucket distribution"
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    viewer_protocol_policy = "redirect-to-https"
+    allowed_methods        = ["GET", "HEAD", "OPTIONS"]
+    cached_methods         = ["GET", "HEAD"]
+    target_origin_id       = "S3Origin"
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
