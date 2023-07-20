d58a584211fe30f0745591b128f87c1211e5febf
Guy Peer <guypeer1985@gmail.com>
2023-07-20 09:59:36 +0300

    full green

--- server/app.py
+++ server/app.py
@@ -15,15 +15,10 @@ formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
 file_handler.setFormatter(formatter)
 logger.addHandler(file_handler)
 
-@app.route('/', methods=['GET'])
-def root():
-    logger.info('Received request for /')
-    return jsonify({"message": "Hello from root!!!!"}), 200
-
 @app.route('/api/message', methods=['GET'])
 def home():
     logger.info('Received request for /api/message')
-    return jsonify({"message": "Hello from server! final call 1"}), 200
+    return jsonify({"message": "Hello from server! finish with apply"}), 200
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
